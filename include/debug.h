33dbce1d239fd7fda77ed1b991a2afc009a8aee0
Tengs Penkwe <tengspenkwe@gmail.com>
2023-12-22 07:12:15 -0800

    copy debug from barrelfish OS

---
+++ include/debug.h
@@ -0,0 +1,47 @@
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+#include <log.h>
+#include <errors/errno.h>
+
+__BEGIN_DECLS
+
+void debug_err(const char *file, const char *func, int line, errval_t err, const char *msg, ...);
+
+void user_panic_fn(const char *file, const char *func, int line, const char *msg, ...)
+    __attribute__((noreturn));
+
+#ifdef NDEBUG
+#define DEBUG_ERR(err, msg, ...) ((void)(0 && err))
+#else
+#define DEBUG_ERR(err, msg, ...) debug_err(__FILE__, __func__, __LINE__, err, msg)
+#endif
+
+#define PUSH_ERR_PRINT(ERR, ERRNAME, fmt, ...) \
+    if(err_is_fail(ERR)) {  \
+        DEBUG_ERR(ERR, fmt, ##__VA_ARGS__); \
+        return err_push(ERR, ERRNAME); \
+    } 
+#define RETURN_ERR_PRINT(ERR, fmt, ...) \
+    if(err_is_fail(ERR)) {  \
+        DEBUG_ERR(ERR, fmt, ##__VA_ARGS__); \
+        return ERR; \
+    } 
+
+/**
+ * @brief Prints out a message and then aborts the domain
+ */
+#define USER_PANIC_ERR(err, msg, ...)                                                                \
+    do {                                                                                           \
+        debug_err(__FILE__, __func__, __LINE__, err, msg, ##__VA_ARGS__);                                         \
+        abort();                                                                                   \
+    } while (0)
+
+/**
+ * @brief Prints out a message and then aborts the domain
+ */
+#define USER_PANIC(msg, ...) user_panic_fn(__FILE__, __func__, __LINE__, msg, ##__VA_ARGS__);
+
+__END_DECLS
+
+#endif // __DEBUG_H__
---
+++ src/errors/debug.c
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2008-2011, ETH Zurich.
+ * Copyright (c) 2015, 2016 Hewlett Packard Enterprise Development LP.
+ * Copyright (c) 2022, The University of British Columbia.
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
+ * ETH Zurich D-INFK, Universitaetstr. 6, CH-8092 Zurich. Attn: Systems Group.
+ */
+#include <debug.h>
+
+#include <stdarg.h>         //va_list, va_start, va_end
+#include <stdio.h>          //snprintf
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Debug prints
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * @brief Prints out the message with the location and error information
+ *
+ * @param[in] file  Source file of the caller
+ * @param[in] func  Function name of the caller
+ * @param[in] line  Line number of the caller
+ * @param[in] err   error number
+ * @param[in] msg   Message format to be printed (printf style)
+ *
+ * @note: do not use this function directly. Rather use the DEBUG_ERR macro
+ */
+void debug_err(const char *file, const char *func, int line, errval_t err, const char *msg, ...)
+{
+    va_list ap;
+    char    str[256];
+    size_t  len;
+
+    char *leader = err_is_ok(err) ? "\x1B[1;32mSUCCESS" : "\x1B[1;91mERROR";
+
+    len = snprintf(str, sizeof(str), "%s:%s() %s:%d\n%s: ", leader, func, file, line, leader);
+    if (msg != NULL) {
+        va_start(ap, msg);
+        len += vsnprintf(str + len, sizeof(str) - len, msg, ap);
+        va_end(ap);
+    }
+
+    len += snprintf(str + len, sizeof(str) - len, "\x1B[0m\n");
+    sys_print(str, len);
+
+    if (err != 0) {
+        leader = "Error calltrace:\n";
+        sys_print(leader, strlen(leader));
+        err_print_calltrace(err);
+        sys_print("\n", 1);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// PANIC
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * @brief Prints a message and aborts the program
+ *
+ * @param[in] file  Source file of the caller
+ * @param[in] func  Function name of the caller
+ * @param[in] line  Line number of the caller
+ * @param[in] msg   Message to print (printf style)
+ *
+ * Something irrecoverably bad happened. Print a panic message, then abort.
+ *
+ * Use the PANIC macros instead of calling this function directly.
+ */
+void user_panic_fn(const char *file, const char *func, int line, const char *msg, ...)
+{
+    va_list ap;
+    char msg_str[128];
+    va_start(ap, msg);
+    vsnprintf(msg_str, sizeof(msg_str), msg, ap);
+    va_end(ap);
+
+    char str[256];
+    snprintf(str, sizeof(str), "\x1B[1;91m%s() %s:%d\n%s\x1B[0m\n", func, file, line, msg_str);
+    sys_print(str, sizeof(str));
+
+    abort();
+}
