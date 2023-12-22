0c67dfb634b2b9c923d4318d4b808b771bed2395
Tengs Penkwe <tengspenkwe@gmail.com>
2023-12-22 03:01:45 -0800

    finish the log system

--- include/log/log.h
+++ include/log/log.h
@@ -5,35 +5,120 @@
 #include <stdarg.h>
 #include <string.h>     //strrchr
 
-// Log Level Implementation for Debug Prints in Barrelfish OS
-// In order to facilitate various levels of logging such as info, debug, and error, a log level system can be implemented. This system allows developers to specify a log level for each message and configure the system to only output messages at or above a certain level.
+__BEGIN_DECLS
+
+// X-macro lists for log levels and modules
+#define LOG_LEVELS \
+    X(VERBOSE, LOG_LEVEL_VERBOSE) \
+    X(INFO,    LOG_LEVEL_INFO)    \
+    X(DEBUG,   LOG_LEVEL_DEBUG)   \
+    X(NOTE,    LOG_LEVEL_NOTE)    \
+    X(WARN,    LOG_LEVEL_WARN)    \
+    X(ERR,     LOG_LEVEL_ERROR)   \
+    X(NONE,    LOG_LEVEL_NONE) 
+
+//Allows developers to specify a log level for each message and configure the system 
+// to only output messages at or above a certain level.
 enum log_level {
-    LOG_LEVEL_TRIVIAL = 0,       // Unimportant
-    LOG_LEVEL_DEBUG = 1,         // Debug messages
-    LOG_LEVEL_INFO = 2,          // Informational messages
-    LOG_LEVEL_WARN = 3,          // Warning messages
-    LOG_LEVEL_ERROR = 4,         // Critical errors
-    LOG_LEVEL_NONE = 5,          // No logs
+#define X(level, def) def,
+    LOG_LEVELS
+#undef  X
 };
+
 // Current Log Level Setting
 #define CURRENT_LOG_LEVEL LOG_LEVEL_DEBUG
 
+#define LOG_MODULE_LEVELS \
+    X(LOG,               CURRENT_LOG_LEVEL) \
+    X(DRIVER,            CURRENT_LOG_LEVEL) \
+    X(ETHER,             CURRENT_LOG_LEVEL) \
+    X(ARP,               CURRENT_LOG_LEVEL) \
+    X(IP,                CURRENT_LOG_LEVEL) \
+    X(ICMP,              CURRENT_LOG_LEVEL) \
+    X(UDP,               CURRENT_LOG_LEVEL) \
+    X(TCP,               CURRENT_LOG_LEVEL)
+
+enum log_module {
+#define X(module, level) module,
+    LOG_MODULE_LEVELS
+#undef X
+    LOG_MODULE_COUNT,
+};
+
+extern enum log_level log_matrix[LOG_MODULE_COUNT];
+
 #define __BASEFILE__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
-// Function Prototypes
-void log_printf(enum log_level level, int line, const char* func, const char* file, const char *msg, ...);
+void log_printf(enum log_module module, enum log_level level, int line, const char* func, const char* file, const char *msg, ...);
 
-#define LOG_PRINT(level, fmt, ...) \
-    do { \
-        if(level >= CURRENT_LOG_LEVEL) { \
-            log_printf(level, __LINE__, __func__, __BASEFILE__, fmt, ##__VA_ARGS__); \
-        } \
-    } while(0)
+#define LOG(module, level, fmt, ...) \
+    if(level >= log_matrix[module]) { \
+        log_printf(module, level, __LINE__, __func__, __BASEFILE__, fmt, ##__VA_ARGS__); \
+    }
 
-#define LOG_TRIVIAL(fmt, ...) LOG_PRINT(LOG_LEVEL_TRIVIAL, fmt, ##__VA_ARGS__)
-#define LOG_DEBUG(fmt, ...)   LOG_PRINT(LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
-#define LOG_INFO(fmt, ...)    LOG_PRINT(LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
-#define LOG_WARN(fmt, ...)    LOG_PRINT(LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
-#define LOG_ERR(fmt, ...)     LOG_PRINT(LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
+#define LOG_VERBOSE(fmt, ...)         LOG(LOG, LOG_LEVEL_VERBOSE, fmt, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...)            LOG(LOG, LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
+#define LOG_DEBUG(fmt, ...)           LOG(LOG, LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
+#define LOG_NOTE(fmt, ...)            LOG(LOG, LOG_LEVEL_NOTE, fmt, ##__VA_ARGS__)
+#define LOG_WARN(fmt, ...)            LOG(LOG, LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
+#define LOG_ERR(fmt, ...)             LOG(LOG, LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
+
+// Define logging macros for DRIVER module
+#define DRIVER_VERBOSE(fmt, ...)      LOG(DRIVER, LOG_LEVEL_VERBOSE, fmt, ##__VA_ARGS__)
+#define DRIVER_INFO(fmt, ...)         LOG(DRIVER, LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
+#define DRIVER_DEBUG(fmt, ...)        LOG(DRIVER, LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
+#define DRIVER_NOTE(fmt, ...)         LOG(DRIVER, LOG_LEVEL_NOTE, fmt, ##__VA_ARGS__)
+#define DRIVER_WARN(fmt, ...)         LOG(DRIVER, LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
+#define DRIVER_ERR(fmt, ...)          LOG(DRIVER, LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
+
+// Define logging macros for ETHER module
+#define ETHER_VERBOSE(fmt, ...)       LOG(ETHER, LOG_LEVEL_VERBOSE, fmt, ##__VA_ARGS__)
+#define ETHER_INFO(fmt, ...)          LOG(ETHER, LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
+#define ETHER_DEBUG(fmt, ...)         LOG(ETHER, LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
+#define ETHER_NOTE(fmt, ...)          LOG(ETHER, LOG_LEVEL_NOTE, fmt, ##__VA_ARGS__)
+#define ETHER_WARN(fmt, ...)          LOG(ETHER, LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
+#define ETHER_ERR(fmt, ...)           LOG(ETHER, LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
+
+// Define logging macros for ARP module
+#define ARP_VERBOSE(fmt, ...)         LOG(ARP, LOG_LEVEL_VERBOSE, fmt, ##__VA_ARGS__)
+#define ARP_INFO(fmt, ...)            LOG(ARP, LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
+#define ARP_DEBUG(fmt, ...)           LOG(ARP, LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
+#define ARP_NOTE(fmt, ...)            LOG(ARP, LOG_LEVEL_NOTE, fmt, ##__VA_ARGS__)
+#define ARP_WARN(fmt, ...)            LOG(ARP, LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
+#define ARP_ERR(fmt, ...)             LOG(ARP, LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
+
+// Define logging macros for IP module
+#define IP_VERBOSE(fmt, ...)          LOG(IP, LOG_LEVEL_VERBOSE, fmt, ##__VA_ARGS__)
+#define IP_INFO(fmt, ...)             LOG(IP, LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
+#define IP_DEBUG(fmt, ...)            LOG(IP, LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
+#define IP_NOTE(fmt, ...)             LOG(IP, LOG_LEVEL_NOTE, fmt, ##__VA_ARGS__)
+#define IP_WARN(fmt, ...)             LOG(IP, LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
+#define IP_ERR(fmt, ...)              LOG(IP, LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
+
+// Define logging macros for ICMP module
+#define ICMP_VERBOSE(fmt, ...)        LOG(ICMP, LOG_LEVEL_VERBOSE, fmt, ##__VA_ARGS__)
+#define ICMP_INFO(fmt, ...)           LOG(ICMP, LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
+#define ICMP_DEBUG(fmt, ...)          LOG(ICMP, LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
+#define ICMP_NOTE(fmt, ...)           LOG(ICMP, LOG_LEVEL_NOTE, fmt, ##__VA_ARGS__)
+#define ICMP_WARN(fmt, ...)           LOG(ICMP, LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
+#define ICMP_ERR(fmt, ...)            LOG(ICMP, LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
+
+// Define logging macros for UDP module
+#define UDP_VERBOSE(fmt, ...)         LOG(UDP, LOG_LEVEL_VERBOSE, fmt, ##__VA_ARGS__)
+#define UDP_INFO(fmt, ...)            LOG(UDP, LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
+#define UDP_DEBUG(fmt, ...)           LOG(UDP, LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
+#define UDP_NOTE(fmt, ...)            LOG(UDP, LOG_LEVEL_NOTE, fmt, ##__VA_ARGS__)
+#define UDP_WARN(fmt, ...)            LOG(UDP, LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
+#define UDP_ERR(fmt, ...)             LOG(UDP, LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
+
+// Define logging macros for TCP module
+#define TCP_VERBOSE(fmt, ...)         LOG(TCP, LOG_LEVEL_VERBOSE, fmt, ##__VA_ARGS__)
+#define TCP_INFO(fmt, ...)            LOG(TCP, LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
+#define TCP_DEBUG(fmt, ...)           LOG(TCP, LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
+#define TCP_NOTE(fmt, ...)            LOG(TCP, LOG_LEVEL_NOTE, fmt, ##__VA_ARGS__)
+#define TCP_WARN(fmt, ...)            LOG(TCP, LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
+#define TCP_ERR(fmt, ...)             LOG(TCP, LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
+
+__END_DECLS
 
 #endif  //__LOG_H__
--- src/log/log.c
+++ src/log/log.c
@@ -1,37 +1,70 @@
 #include <log/log.h>
 #include <stdio.h>
 
-void log_printf(enum log_level level, int line, const char* func, const char* file, const char *msg,  ...) 
-{
-    (void) level;
-    (void) line;
-    (void) func;
-    (void) file;
-    (void) msg;
-    // va_list ap;
-    // char    str[256];
+enum log_level log_matrix[LOG_MODULE_COUNT] = {
+#define X(module, level) level,
+    LOG_MODULE_LEVELS
+#undef X
+};
 
-    // const char *level_colors[] = {
-    //     [LOG_LEVEL_TRIVIAL] = "[Trivial]", 
-    //     [LOG_LEVEL_DEBUG]   = "\x1B[1m[Debug]", 
-    //     [LOG_LEVEL_INFO]    = "\x1B[0;34m[Info]", // Blue
-    //     [LOG_LEVEL_WARN]    = "\x1B[1;33m[Warn]", // Yellow
-    //     [LOG_LEVEL_ERROR]   = "\x1B[1;91m[Error]"  // Red
-    // };
-
-    // const char *leader = level < LOG_LEVEL_NONE ? level_colors[level] : "Unknown";
-
-    // size_t len = snprintf(str, sizeof(str), "%s(%.*s.%u.%u) %d:%s()->%s: ", leader, DISP_NAME_LEN,
-    //                disp_name(), disp_get_current_core_id(), disp_get_domain_id(), line, func, file, leader);
-    // if (msg != NULL) {
-    //     va_start(ap, msg);
-    //     len += vsnprintf(str + len, sizeof(str) - len, msg, ap);
-    //     va_end(ap);
-    // }
-
-    // const char *tail = level < LOG_LEVEL_DEBUG ? "\n" : "\x1B[0m\n";
-
-    // len += snprintf(str + len, sizeof(str) - len, tail);
-    // printf(str);
+// Utility function to convert log level enum to string
+const char* level_to_string(enum log_level level) {
+    switch (level) {
+        case LOG_LEVEL_VERBOSE: return "VERBOSE";
+        case LOG_LEVEL_INFO:    return "INFO ";
+        case LOG_LEVEL_DEBUG:   return "DEBUG";
+        case LOG_LEVEL_NOTE:    return "NOTE ";
+        case LOG_LEVEL_WARN:    return "WARN ";
+        case LOG_LEVEL_ERROR:   return "ERROR";
+        default:                return "UNKNOWN";
+    }
 }
 
+const char *level_colors[] = {
+    [LOG_LEVEL_VERBOSE] = "", 
+    [LOG_LEVEL_INFO]    = "\x1B[1m", 
+    [LOG_LEVEL_DEBUG]   = "\x1B[0;34m", 
+    [LOG_LEVEL_NOTE]    = "\x1B[1;34m", // Blue
+    [LOG_LEVEL_WARN]    = "\x1B[0;33m", // Yellow
+    [LOG_LEVEL_ERROR]   = "\x1B[0;91m"  // Red
+};
+
+// Utility function to convert log module enum to string
+const char* module_to_string(enum log_module module) {
+    switch (module) {
+        case LOG:       return "LOG";
+        case DRIVER:    return "DRIVER";
+        case ETHER:     return "ETHER";
+        case ARP:       return "ARP";
+        case IP:        return "IP";
+        case ICMP:      return "ICMP";
+        case UDP:       return "UDP";
+        case TCP:       return "TCP";
+        default:        return "UNKNOWN_MODULE";
+    }
+}
+
+void log_printf(enum log_module module, enum log_level level, int line, const char* func, const char* file, const char *msg, ...)
+{
+    va_list ap;
+    static char buffer[1024]; 
+
+    const char *leader = level < LOG_LEVEL_NONE ? level_colors[level] : "Unknown";
+
+    // Format the log prefix with level, module, file, line, and function
+    int offset = snprintf(buffer, sizeof(buffer), "%s[%s-%s] %s:%d:%s(): ", leader, level_to_string(level), module_to_string(module), file, line, func);
+
+    // Append the formatted message
+    if (msg != NULL && offset < (int)sizeof(buffer)) {
+        va_start(ap, msg);
+        vsnprintf(buffer + offset, sizeof(buffer) - offset, msg, ap);
+        va_end(ap);
+    }
+
+    // Reset color at the end of the message
+    const char *tail = level < LOG_LEVEL_INFO ? "\n" : "\x1B[0m\n";
+    strncat(buffer, tail, sizeof(buffer) - strlen(buffer) - 1);
+
+    // Print the log message
+    printf("%s", buffer);
+}
