06a2a2a6522cb8d2d6d6c83302de228b67bdb647
Tengs Penkwe <tengspenkwe@gmail.com>
2023-12-26 00:33:30 -0800

    add global driver structure

--- include/driver.h
+++ include/driver.h
@@ -3,10 +3,24 @@
 
 #include <common.h>
 
+#include <netstack/ethernet.h>
+#include <device/device.h>
+#include <event/threadpool.h>
+
 // Each pieces have reserved space in the head to avoid copying
 #define  MEMPOOL_BYTES       ETHER_MAX_SIZE + DEVICE_HEADER_RESERVE
 // Give 2048 peices in Memory pool
 #define  MEMPOOL_AMOUNT      2048
+
+typedef struct driver {
+    Ethernet    *ether;
+    NetDevice   *device;  
+    MemPool     *mempool;
+    ThreadPool  *threadpool;
+
+} Driver ;
+
+extern Driver g_driver;
 
 __BEGIN_DECLS
 
