cc130255dc9ca3494a09023deedcd6809936d879
Tengs Penkwe <tengspenkwe@gmail.com>
2023-12-27 08:22:50 -0800

    IMPROVE: add state.c in event to transfer global states

---
+++ include/event/states.h
@@ -0,0 +1,32 @@
+#ifndef __EVENT_STATES_H__
+#define __EVENT_STATES_H__
+
+#include <common.h>
+#include <time.h>
+#include <netstack/ethernet.h>
+#include <device/device.h>
+#include <lock_free/memorypool.h>
+#include <event/threadpool.h>
+
+typedef struct global_states {
+    /// @brief For TCP 
+    size_t          max_worker_for_single_tcp_server;
+
+    struct {
+        Ethernet    *ether;
+        NetDevice   *device;  
+        MemPool     *mempool;
+        ThreadPool  *threadpool;
+    } driver;
+    /// @brief For Device
+    size_t          recvd;   ///< How many packets have we received
+    size_t          fail_process;
+    size_t          sent;    // Maybe inaccurate because multi-threading
+    size_t          fail_sent;
+    struct timespec start_time;
+
+} GlobalStates;
+
+extern GlobalStates g_states;
+
+#endif // __EVENT_STATES_H__
