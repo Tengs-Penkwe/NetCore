8b4f32dc37e57f42298e416da17073336567d6d7
Tengs Penkwe <tengspenkwe@gmail.com>
2024-01-01 20:13:43 -0800

    This commit updates the checksum calculation logic in a networking project to support both IPv4 and IPv6 protocols. It introduces separate structures within `pseudo_ip_header_in_net_order` for IPv4 and IPv6, along with `static_assert` checks for their sizes. The TCP and UDP checksum functions are modified to handle these structures based on the IP protocol version.

--- include/netutil/checksum.h
+++ include/netutil/checksum.h
@@ -8,16 +8,30 @@
  */
 uint16_t inet_checksum(void *dataptr, uint16_t len);
 
+#include <netutil/ip.h>
 
 struct pseudo_ip_header_in_net_order {
-    uint32_t src_addr;
-    uint32_t dst_addr;
-    uint8_t  reserved;  // This is typically 0
-    uint8_t  protocol;
-    uint16_t len_no_iph;
+    union {
+        struct {
+            uint32_t src_addr;
+            uint32_t dst_addr;
+            uint8_t  reserved;  // This is typically 0
+            uint8_t  protocol;
+            uint16_t len_no_iph;
+        }__attribute__((packed)) ipv4;
+        struct {
+            ipv6_addr_t src_addr;
+            ipv6_addr_t dst_addr;
+            uint32_t    len_no_iph;   
+            uint8_t     zero[3];      // Reserved 3-byte field set to zero
+            uint8_t     next_header;  // Next Header field identifying the TCP protocol (8 bits)
+        } __attribute__((packed)) ipv6;
+    };
+    bool is_ipv6;
 } __attribute__((packed));
 
-static_assert(sizeof(struct pseudo_ip_header_in_net_order) == 12, "pseudo_ip_header_in_net_order has wrong size");
+static_assert(sizeof(((struct pseudo_ip_header_in_net_order *)0)->ipv4) == 12, "Size of ipv4 sub-structure is not as expected (12)");
+static_assert(sizeof(((struct pseudo_ip_header_in_net_order *)0)->ipv6) == 40, "Size of ipv6 sub-structure is not as expected (40)");
 
 /**
  * Calculate the UDP checksum according to RFC1071
@@ -29,6 +43,29 @@ uint16_t udp_checksum_in_net_order(const void *data_no_iph, struct pseudo_ip_hea
  */
 uint16_t tcp_checksum_in_net_order(const void *data_no_iph, struct pseudo_ip_header_in_net_order pheader);
 
+#define PSEUDO_HEADER_IPv4(ipv4_src, ipv4_dst, proto, len) \
+    (struct pseudo_ip_header_in_net_order) { \
+        .ipv4 = { \
+            .src_addr    = htons(ipv4_src), \
+            .dst_addr    = htons(ipv4_dst), \
+            .reserved    = 0, \
+            .protocol    = proto, \
+            .len_no_iph  = htons((uint16_t)len), \
+        }, \
+        .is_ipv6 = false, \
+    }
+
+#define PSEUDO_HEADER_IPv6(ipv6_src, ipv6_dst, next, len) \
+    (struct pseudo_ip_header_in_net_order) { \
+        .ipv6 = { \
+            .src_addr    = hton16(ipv6_src), \
+            .dst_addr    = hton16(ipv6_dst), \
+            .len_no_iph  = htonl((uint32_t)len), \
+            .zero        = { 0 }, \
+            .next_header = next, \
+        }, \
+        .is_ipv6 = true, \
+    }
 
 #endif
 
--- src/netutil/checksum.c
+++ src/netutil/checksum.c
@@ -64,12 +64,23 @@ static uint32_t part_checksum(const void *buf, uint16_t len) {
 
 uint16_t tcp_checksum_in_net_order(const void *data_no_iph, struct pseudo_ip_header_in_net_order pheader) {
 
-    uint16_t whole_len_no_iph = ntohs(pheader.len_no_iph);
-    assert(pheader.reserved == 0);
-    assert(sizeof(pheader) % 2 == 0);
-    
     uint32_t sum = 0;
-    sum += part_checksum(&pheader, sizeof(pheader));
+    uint16_t whole_len_no_iph;
+    if (pheader.is_ipv6)
+    {
+        whole_len_no_iph = ntohl(pheader.ipv6.len_no_iph);
+        assert(memcmp(&pheader.ipv6.zero, "\x00\x00\x00", sizeof(pheader.ipv6.zero)) == 0);
+        assert(sizeof(pheader.ipv6) % 2 == 0);
+        sum += part_checksum(&pheader, sizeof(pheader.ipv6));
+    }
+    else
+    {
+        whole_len_no_iph = ntohs(pheader.ipv4.len_no_iph);
+        assert(pheader.ipv4.reserved == 0);
+        assert(sizeof(pheader.ipv4) % 2 == 0);
+        sum += part_checksum(&pheader, sizeof(pheader.ipv4));
+    }
+    
     sum += part_checksum(data_no_iph, whole_len_no_iph);
 
     sum  = (sum >> 16) + (sum & 0xFFFF);
@@ -80,7 +91,6 @@ uint16_t tcp_checksum_in_net_order(const void *data_no_iph, struct pseudo_ip_hea
 }
 
 uint16_t udp_checksum_in_net_order(const void *data_no_iph, struct pseudo_ip_header_in_net_order pheader) {
-    uint16_t checksum_maybe_zero = tcp_checksum_in_net_order(data_no_iph, pheader);
-    uint16_t checksum_no_zero = (checksum_maybe_zero == 0x0000) ? 0xFFFF : checksum_maybe_zero;
-    return checksum_no_zero;
+    uint16_t checksum = tcp_checksum_in_net_order(data_no_iph, pheader);
+    return (checksum == 0x0000) ? 0xFFFF : checksum;
 }
